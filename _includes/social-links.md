0ab1c47db4144768269e18d434ebfc72f41ed238
Aaron Ault <ault@purdue.edu>
2014-03-23 00:32:47 -0400

    oada version check done for new blog post on working groups

--- _includes/social-links.md
+++
@@ -1,3 +0,0 @@
-<ul class="social">
-			<li><a href="https://github.com/OADA"><i class="fa fa-github"></i></a></li>
-      </ul>
--- js/init.js
+++
@@ -1,9 +0,0 @@
-$(document).foundation({
-        magellan : {
-           active_class: 'active',
-           threshold: 0,
-           throttle_delay: 50, 
-            destination_threshold: 100
-		   }
-    });
-  svgeezy.init("nocheck","png");
